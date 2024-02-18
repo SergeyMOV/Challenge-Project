f8f851d01511dae787f57bdda93c2e88b12cd16a
Sergey Movsesyan <sm877141@gmail.com>
2024-02-18 23:33:10 +0100

    fixed some issues

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const passwordShowButton = document.querySelector('[data-js ="eye-button"]');
 const passwordText = document.querySelector(".passwordField");
 const form = document.querySelector("form");
 
+passwordShowButton.classList.add("fa", "fa-eye-slash");
 
 passwordShowButton.addEventListener("click",()=>{
-if(passwordText.type==="password"){
+if(passwordText.type ==="password"){
     passwordText.type="text";
-    
+    passwordShowButton.classList.remove("fa-eye-slash");
+    passwordShowButton.classList.add("fa-eye");
 }
 else{
-passwordText.type="password";
+passwordText.type ="password";
+passwordShowButton.classList.remove("fa-eye");
+passwordShowButton.classList.add("fa-eye-slash");
 }
-})
+});
 
 
 form.addEventListener('submit', function (e) {
